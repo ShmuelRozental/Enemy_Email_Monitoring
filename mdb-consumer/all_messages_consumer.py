ede034868c52e706f43e90b2ec3df93593622876
ShmuelRozental <rozentalshmuel@gmail.com>
2024-11-14 19:06:25 +0200

    fix the bootstrap_servers

--- mdb-consumer/all_messages_consumer.py
+++ mdb-consumer/all_messages_consumer.py
@@ -1,16 +1,17 @@
 from kafka import KafkaConsumer
 from pymongo import MongoClient
+import os
 import json
 
 
 consumer = KafkaConsumer(
     'all_emails',
-    bootstrap_servers='localhost:9092',
+    bootstrap_servers=(os.getenv("KAFKA_BOOTSTRAP_SERVERS", "kafka:9092")),
     value_deserializer=lambda v: json.loads(v.decode('utf-8'))
 )
 
 
-mongo_client = MongoClient('localhost', 27017)
+mongo_client = MongoClient('kafka', 27017)
 db = mongo_client['email_monitor']
 collection = db['all_messages']
 
